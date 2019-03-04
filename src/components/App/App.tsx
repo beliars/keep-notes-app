bb484dc6ffb57f6106d8a330de7acaf01ceae902
Dmitriy Melnichenko <tairezzzz@gmail.com>
2019-03-04 17:16:15 +0200

    feat (components) add header component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,28 +2,16 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
 import './App.scss';
-import logo from '../../logo.svg';
 import store from '../../redux/store';
+import Header from '../Header/Header';
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <div className="app">
-          <header className="app-header">
-            <img src={logo} className="app-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-              className="app-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+          <Header />
+        
         </div>
       </Provider>
     );
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './Header.scss';
+import logo from '../../logo.svg';
+
+const Header = () => {
+    return (
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="app-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    );
+};
+
+export default Header;
