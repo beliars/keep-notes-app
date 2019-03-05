6ae9e4b2dd0b9bc2b03099d1856614bd8eb9d8d5
Dmitriy Melnichenko <tairezzzz@gmail.com>
2019-03-05 17:09:25 +0200

    feat (components) update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,72 @@
-import React from 'react';
+//import React from 'react';
+//
+//import './Header.scss';
+//import Button from '@material-ui/core/Button/Button';
+//
+//const Header = () => {
+//    return (
+//      <header className="app-header">
+//        <Button variant="contained" color="primary">
+//          Hello World
+//        </Button>
+//      </header>
+//    );
+//};
+//
+//export default Header;
 
-import './Header.scss';
-import logo from '../../logo.svg';
 
-const Header = () => {
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const styles = createStyles({
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+});
+
+export interface Props extends WithStyles<typeof styles> {}
+
+interface State {
+  open: boolean;
+}
+
+class Header extends Component<Props, State> {
+  state = {
+    open: false,
+  };
+  render() {
+    const { classes } = this.props;
+    const { open } = this.state;
     return (
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`${classes.root} app-header`}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Keep notes
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
     );
-};
+  };
+}
 
-export default Header;
+export default withStyles(styles)(Header);
