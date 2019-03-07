8605d1a419c0a9a5980a6d8e48b0c06f9e43bbb9
Dmitriy Melnichenko <tairezzzz@gmail.com>
2019-03-07 10:43:51 +0200

    feat (public) change app title

---
+++ src/pages/Trash/Trash.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import './SignIn.scss';
+
+export default class SignIn extends Component {
+  render() {
+    return (
+      <div className="signin-page">
+        Sign in page
+      </div>
+    );
+  }
+}
