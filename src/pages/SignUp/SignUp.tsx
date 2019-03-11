6595c39618e7c0b86f270af77e42e2c1162fddfb
Dmitriy Melnichenko <tairezzzz@gmail.com>
2019-03-11 11:25:02 +0200

    feat (pages) add SignUpForm component

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 
 import './SignUp.scss';
+import SignUpForm from './SignUpForm/SignUpForm';
 
 export default class SignUp extends Component {
   render() {
     return (
       <div className="signup-page">
         Sign up page
+        <SignUpForm isLoading={false}/>
       </div>
     );
   }
--- src/pages/SignUp/SignUpForm/SignUpForm.tsx
+++ src/pages/SignUp/SignUpForm/SignUpForm.tsx
@@ -1,13 +1,44 @@
-import React, {Component} from 'react';
+import React from 'react'
+import { Field, InjectedFormProps, reduxForm } from 'redux-form'
 
-import './SignUp.scss';
+import FormField from '../../../shared/components/FormField/FormField';
 
-export default class SignUp extends Component {
-  render() {
-    return (
-      <div className="signup-page">
-        Sign up page
-      </div>
-    );
-  }
+export interface OwnProps {
+  isLoading: boolean,
 }
+
+export interface SignUpFormData {
+  email: string,
+  password: string,
+}
+
+type Props = OwnProps & InjectedFormProps;
+
+const SignUpForm = (props: Props) => {
+  const { handleSubmit, pristine, isLoading } = props;
+  return (
+    <form onSubmit={handleSubmit}>
+      <Field
+        name="email"
+        type="email"
+        component={FormField}
+        label="Email"
+      />
+      <Field
+        name="password"
+        type="password"
+        component={FormField}
+        label="Password"
+      />
+      <div>
+        <button type="submit" disabled={pristine || isLoading}>
+          Submit
+        </button>
+      </div>
+    </form>
+  )
+};
+
+export default reduxForm<SignUpFormData, any>({
+  form: 'signUpForm',
+})(SignUpForm);
