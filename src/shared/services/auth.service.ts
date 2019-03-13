10d7b91ab6d02a98922f214bee725225d5c83ff7
Dmitriy Melnichenko <tairezzzz@gmail.com>
2019-03-13 17:57:25 +0200

    feat (services) auth server is updated with set cookies

--- src/shared/services/auth.service.ts
+++ src/shared/services/auth.service.ts
@@ -1,14 +1,20 @@
 import axios from 'axios';
+import {Cookies} from "react-cookie";
 
 import { SignUpFormData } from '../../components/pages/SignUp/SignUpForm/SignUpForm';
-import { from } from 'rxjs';
+import { from, of } from 'rxjs';
 
 const baseUrl = 'http://localhost:2380';
+const cookies = new Cookies();
 
 class AuthService {
  
   signup(data: SignUpFormData) {
     return from(axios.post(`${baseUrl}/signup`, data));
+  }
+  
+  setToken(token: string) {
+    cookies.set('auth_token', token);
   }
 
 }
