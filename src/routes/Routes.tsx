3c2d1937acee719759b1b3b7dd95ae1977bb56cf
Dmitriy Melnichenko <tairezzzz@gmail.com>
2019-03-19 10:56:41 +0200

    feat (routes) update routes with isGuest

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+
+import authService from '../shared/services/auth.service';
 
 import SignUp from '../components/pages/SignUp/SignUp';
 import SignIn from '../components/pages/SignIn/SignIn';
@@ -11,35 +14,71 @@ import NotFound from '../components/pages/NotFound/NotFound';
 
 import { RootState } from '../redux/store';
 import { AuthState } from '../redux/auth/states';
-
+import { ActionTypes as UsersActionTypes } from '../redux/users/actions';
+import { ActionTypes as AuthActionTypes } from '../redux/auth/actions';
 
 interface StateProps {
   auth: AuthState,
 }
 
-type Props = StateProps;
+interface DispatchProps {
+  getSelfData(id: string): void,
+  setGuestIsTrue(): void,
+}
 
-const Routes: React.FunctionComponent<Props> = (props: Props) => {
-  const {auth} = props;
-  const {isGuest} = auth;
-  console.log(isGuest);
-  return (
-    <Router>
-      <main className='main'>
-        <Switch>
-          <Route path="/sign-up" exact component={SignUp}/>
-          <Route path="/sign-in" exact component={SignIn}/>
-          <PrivateRoute path='/' exact component={Home} isGuest={isGuest}/>
-          <PrivateRoute path="/trash" component={Trash} isGuest={isGuest}/>
-          <Route component={NotFound}/>
-        </Switch>
-      </main>
-    </Router>
-  );
-};
+type Props = StateProps & DispatchProps;
+
+class Routes extends Component<Props> {
+  
+  componentDidMount() {
+    const {getSelfData, setGuestIsTrue} = this.props;
+    if (authService.isSetSessionData) {
+      const userId = authService.getSessionId;
+      getSelfData(userId);
+    } else {
+      setGuestIsTrue();
+    }
+  }
+  
+  render() {
+    const {auth} = this.props;
+    const {isGuest} = auth;
+    return (
+      isGuest === null ?
+        null :
+        <Router>
+          <main className='main'>
+            <Switch>
+              <Route path="/sign-up" exact component={SignUp}/>
+              <Route path="/sign-in" exact component={SignIn}/>
+              <PrivateRoute path='/' exact component={Home} isGuest={isGuest}/>
+              <PrivateRoute path="/trash" component={Trash} isGuest={isGuest}/>
+              <Route component={NotFound}/>
+            </Switch>
+          </main>
+        </Router>
+    );
+  };
+}
 
 const mapStateToProps = (state: RootState): StateProps => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps)(Routes);
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => (
+  {
+    getSelfData: (id: string) => {
+      dispatch({
+        type: UsersActionTypes.GET_SELF_DATA,
+        payload: id
+      });
+    },
+    setGuestIsTrue: () => {
+      dispatch({
+        type: AuthActionTypes.SET_GUEST_IS_TRUE,
+      });
+    },
+  }
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Routes);
