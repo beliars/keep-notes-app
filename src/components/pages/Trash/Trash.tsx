0408ac4b705c424c9e958d27c72cfd1ee1deba84
Dmitriy Melnichenko <tairezzzz@gmail.com>
2019-03-27 12:49:54 +0200

    feat (pages) trash page width update

--- src/components/pages/Trash/Trash.tsx
+++ src/components/pages/Trash/Trash.tsx
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import classNames from 'classnames';
 
 import './Trash.scss';
+import { ApplicationState } from '../../../redux/application/states';
+import { RootState } from '../../../redux/store';
 
-export default class Trash extends Component {
+interface StateProps {
+  application: ApplicationState,
+}
+
+type Props = StateProps;
+
+class Trash extends Component<Props> {
+  
   render() {
+    const {isShowSidebar} = this.props.application;
     return (
-      <div className="trash-page">
+      <div className={classNames('content-page', 'trash-page', isShowSidebar ? 'sidebar-is-opened' : 'sidebar-is-closed')}>
         Trash page
       </div>
     );
   }
 }
+
+const mapStateToProps = (state: RootState): StateProps => ({
+  application: state.application,
+});
+
+export default connect(mapStateToProps)(Trash);
